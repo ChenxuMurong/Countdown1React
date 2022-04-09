de201ebe4857abb712559d0219e7d6ddc0183e9d
ChenxuMurong <99457790+ChenxuMurong@users.noreply.github.com>
2022-04-08 21:39:56 -0400

    yeah

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -13,68 +13,74 @@ import './index.css';
   }
   
   class Board extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            squares : Array(9).fill(null),
-            xIsNext : true,
-        };
-    }
-
-    handleClick(i){
-        const squares = this.state.squares.slice();
-        if (calculateWinner(this.state.squares) || squares[i]){
-            return;
-        }
-        squares[i] = this.state.xIsNext ? 'X' : 'O';
-        this.setState({squares : squares,
-                       xIsNext : !this.state.xIsNext});
-    }
-
     renderSquare(i) {
-      return (
-          <Square value={this.state.squares[i]} 
-              onClick = {() => this.handleClick(i)} 
+          return (
+              <Square value={this.props.squares[i]} 
+              onClick = {() => this.props.onClick(i)} 
               />
-            );
-    }
-  
+              );
+            }
+            
     render() {
-      const winner = calculateWinner(this.state.squares);
-      const status = winner? "winner is " + winner : 'Next player: ' + (this.state.xIsNext? 'X' : 'O');
-  
-      return (
-        <div>
-          <div className="status">{status}</div>
-          <div className="board-row">
-            {this.renderSquare(0)}
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(3)}
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(6)}
-            {this.renderSquare(7)}
-            {this.renderSquare(8)}
-          </div>
-        </div>
-      );
-    }
-  }
-  
-  class Game extends React.Component {
-    render() {
+            return (
+                <div>
+                <div className="board-row">
+                {this.renderSquare(0)}
+                {this.renderSquare(1)}
+                {this.renderSquare(2)}
+                </div>
+                <div className="board-row">
+                {this.renderSquare(3)}
+                {this.renderSquare(4)}
+                {this.renderSquare(5)}
+                </div>
+                <div className="board-row">
+                {this.renderSquare(6)}
+                {this.renderSquare(7)}
+                {this.renderSquare(8)}
+                </div>
+                </div>
+                );
+                }
+            }
+            
+    class Game extends React.Component {
+        constructor(props){
+            super(props);
+            this.state = {
+                history: [{squares : Array(9).fill(null)
+                }],
+                xIsNext : true
+            };
+        }
+
+        handleClick(i){
+            const history = this.state.history;
+            const current = history[history.length - 1];
+            if (calculateWinner(current.squares) || current.squares[i]){
+                return;
+            }
+            current.squares[i] = this.state.xIsNext ? 'X' : 'O';
+            this.setState({squares : current.squares,
+                            xIsNext : !this.state.xIsNext});
+        }
+
+        render() {
+            const history = this.state.history;
+            const current = history[history.length - 1];
+            const winner = calculateWinner(current.squares);
+            const status = winner? "winner is " + 
+        winner : 'Next player: ' + (this.state.xIsNext? 'X' : 'O');
+
       return (
         <div className="game">
           <div className="game-board">
-            <Board />
+            <Board squares = {current.squares}
+                    onClick = {(i) => this.handleClick(i)}
+                    />
           </div>
           <div className="game-info">
-            <div>{/* status */}</div>
+            <div>{status}</div>
             <ol>{/* TODO */}</ol>
           </div>
         </div>
