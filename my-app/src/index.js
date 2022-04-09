b31b4b811e7f111d31a8095e04867b4278042795
ChenxuMurong <99457790+ChenxuMurong@users.noreply.github.com>
2022-04-08 23:42:43 -0400

    added time travel buttons

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-
+  // function component since it doesn't have its 
+  // own state
   function Square(props){
     return (
+        // onClick method can be passed from Board
         <button className="square" 
                 onClick={props.onClick}>
           {props.value}
@@ -16,37 +18,38 @@ import './index.css';
     renderSquare(i) {
           return (
               <Square value={this.props.squares[i]} 
-              onClick = {() => this.props.onClick(i)} 
+                      onClick = {() => this.props.onClick(i)} 
               />
               );
             }
             
     render() {
-            return (
-                <div>
-                <div className="board-row">
-                {this.renderSquare(0)}
-                {this.renderSquare(1)}
-                {this.renderSquare(2)}
-                </div>
-                <div className="board-row">
-                {this.renderSquare(3)}
-                {this.renderSquare(4)}
-                {this.renderSquare(5)}
-                </div>
-                <div className="board-row">
-                {this.renderSquare(6)}
-                {this.renderSquare(7)}
-                {this.renderSquare(8)}
-                </div>
-                </div>
+        return (
+            <div>
+              <div className="board-row">
+              {this.renderSquare(0)}
+              {this.renderSquare(1)}
+              {this.renderSquare(2)}
+              </div>
+              <div className="board-row">
+              {this.renderSquare(3)}
+              {this.renderSquare(4)}
+              {this.renderSquare(5)}
+              </div>
+              <div className="board-row">
+              {this.renderSquare(6)}
+              {this.renderSquare(7)}
+              {this.renderSquare(8)}
+              </div>
+            </div>
                 );
                 }
             }
             
-    class Game extends React.Component {
+class Game extends React.Component {
         constructor(props){
             super(props);
+            // game has full control over history
             this.state = {
                 history: [{squares : Array(9).fill(null)
                 }],
@@ -57,33 +60,66 @@ import './index.css';
         handleClick(i){
             const history = this.state.history;
             const current = history[history.length - 1];
-            if (calculateWinner(current.squares) || current.squares[i]){
+            // name improved from "squares": otherwise
+            // it will be too confusing. 
+            // Just keep in mind that "current" refers to
+            // an object (previously known as state) in history.
+            const currSquares = current.squares.slice();
+
+            // return early if winner has been declared
+            // or if currSquaresp[i], a square, has already
+            // been filled
+            if (calculateWinner(currSquares) || currSquares[i]){
                 return;
             }
-            current.squares[i] = this.state.xIsNext ? 'X' : 'O';
-            this.setState({squares : current.squares,
-                            xIsNext : !this.state.xIsNext});
+            currSquares[i] = this.state.xIsNext ? 'X' : 'O';
+
+            this.setState({history :  history.concat(
+              [{squares : currSquares}]
+            ),
+              xIsNext : !this.state.xIsNext});
         }
 
         render() {
             const history = this.state.history;
             const current = history[history.length - 1];
             const winner = calculateWinner(current.squares);
+            // improvement: using ternary operator to determine 
+            // the status string from current state
             const status = winner? "winner is " + 
         winner : 'Next player: ' + (this.state.xIsNext? 'X' : 'O');
 
+          // returns a list of buttons. Upon clicking, they can
+          // take u back in time
+          // syntax of map: map((element, index) => { /* ... */ })
+            const moves = history.map((step, moveInd)=>{
+                const desc = moveInd ? 
+                              "Go to move #" + moveInd:
+                              "Go to game start";
+                return(
+                  <li>
+                    <button className='historyButton' 
+                    onClick={() => this.jumpTo(moveInd)}>
+                      {desc}
+                    </button>
+                  </li>
+                )
+            });
+
       return (
-        <div className="game">
-          <div className="game-board">
-            <Board squares = {current.squares}
-                    onClick = {(i) => this.handleClick(i)}
-                    />
+
+          <div className="game">
+            <div className="game-board">
+              <Board squares = {current.squares}
+                      onClick = {(i) => this.handleClick(i)}
+                      />
+            </div>
+            <div className="game-info">
+              <div className="game-info-status">{status}</div>
+              <ul>{moves}</ul>
+            </div>
           </div>
-          <div className="game-info">
-            <div>{status}</div>
-            <ol>{/* TODO */}</ol>
-          </div>
-        </div>
+
       );
     }
   }
